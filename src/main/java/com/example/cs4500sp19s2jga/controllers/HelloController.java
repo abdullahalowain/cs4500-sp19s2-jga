9da4a38b93b9a32581565e50e8a943b4cd45320d
jannunzi <jannunzi@gmail.com>
2019-01-11 14:16:11 -0500

    added get hello object and parameter

--- src/main/java/com/example/cs4500sp19s2jga/controllers/HelloController.java
+++ src/main/java/com/example/cs4500sp19s2jga/controllers/HelloController.java
@@ -1,12 +1,29 @@
 package com.example.cs4500sp19s2jga.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.cs4500sp19s2jga.models.HelloObject;
 
 @RestController
 public class HelloController {
 	@GetMapping("/api/hello")
 	public String sayHello() {
 		return "Hello World";
+	}
+
+	@GetMapping("/api/hello/object")
+	public HelloObject getHelloObject() {
+		HelloObject hello = new HelloObject();
+		hello.setMessage("Hello World Object");
+		return hello;
+	}
+	@GetMapping("/api/hello/parameter/{someMessage}")
+	public HelloObject getHelloObjectParameter(
+			@PathVariable("someMessage") String message) {
+		HelloObject hello = new HelloObject();
+		hello.setMessage(message);
+		return hello;
 	}
 }
