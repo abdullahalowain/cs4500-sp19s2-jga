5599a8b63619e9c9879dd1b95e43022ed3d1f2cc
Jose Annunziato <jannunzi@gmail.com>
2019-01-30 23:07:55 -0500

    added service entity

---
+++ src/main/java/com/example/cs4500sp19s2jga/models/Service.java
@@ -0,0 +1,28 @@
+package com.example.cs4500sp19s2jga.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="services")
+public class Service {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
