2c8c51593378716a69ee78e1552961ce8012ca65
Jose Annunziato <jannunzi@gmail.com>
2019-01-30 22:29:05 -0500

    added faq answer

---
+++ src/main/java/com/example/cs4500sp19s2jga/models/FrequentlyAskedAnswer.java
@@ -0,0 +1,28 @@
+package com.example.cs4500sp19s2jga.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="frequently_asked_answers")
+public class FrequentlyAskedAnswer {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+    private String answer;
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getAnswer() {
+        return answer;
+    }
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
