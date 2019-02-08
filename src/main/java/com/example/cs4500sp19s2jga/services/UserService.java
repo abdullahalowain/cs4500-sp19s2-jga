67a4e1b82380fa11ea7bc29d46af678d1cf293be
Jose Annunziato <jannunzi@gmail.com>
2019-02-07 19:33:57 -0500

    added cross origin

--- src/main/java/com/example/cs4500sp19s2jga/services/UserService.java
+++ src/main/java/com/example/cs4500sp19s2jga/services/UserService.java
@@ -3,6 +3,7 @@ package com.example.cs4500sp19s2jga.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import com.example.cs4500sp19s2jga.models.User;
 import com.example.cs4500sp19s2jga.repository.UserRepository;
 
 @RestController
+@CrossOrigin(origins="*")
 public class UserService {
 	@Autowired
 	UserRepository userRepository;
