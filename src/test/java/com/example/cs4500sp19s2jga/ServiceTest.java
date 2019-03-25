04515594ce2282c47428f2bbf4464e9baf6b64a0
Jose Annunziato <jannunzi@gmail.com>
2019-03-24 22:47:03 -0400

    added service mock test

--- src/test/java/com/example/cs4500sp19s2jga/ServiceTest.java
+++ src/test/java/com/example/cs4500sp19s2jga/ServiceTest.java
@@ -24,7 +24,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import com.example.cs4500sp19s2jga.controllers.ServiceController;
 import com.example.cs4500sp19s2jga.models.Service;
 import com.example.cs4500sp19s2jga.models.ServiceCategory;
+import com.example.cs4500sp19s2jga.repository.ServiceRepository;
 import com.example.cs4500sp19s2jga.services.ServiceService;
+import com.example.cs4500sp19s2jga.services.UserService;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(ServiceController.class)
@@ -33,6 +35,10 @@ public class ServiceTest {
     private MockMvc mockMvc;
     @MockBean
     private ServiceService serviceService;
+    @MockBean
+    private UserService userService;
+    @MockBean
+    private ServiceRepository serviceRepository;
     @Test
     public void testFindServiceById() throws Exception {
         ServiceCategory engineering = new ServiceCategory(123, "Engineering");
@@ -42,10 +48,10 @@ public class ServiceTest {
         webDevService.setServiceCategories(categories);
         when(serviceService.findServiceById(345)).thenReturn(webDevService);
         this.mockMvc
-            .perform(get("/api/services/123"))
+            .perform(get("/api/services/345"))
             .andDo(print())
             .andExpect(status().isOk())
-            .andExpect(jsonPath("$.id", is(123)))
+            .andExpect(jsonPath("$.id", is("345")))
             .andExpect(jsonPath("$.serviceCategories", hasSize(2)))
             .andExpect(jsonPath("$.serviceCategories[*].title", containsInAnyOrder("Engineering", "Training")));
     }
